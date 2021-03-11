b0724d2b15afba165c6b2af750b2beb4e0d6d98e
raion167 <joaopedroengmec@hotmail.com>
2021-03-10 21:06:52 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+/*<label class="todo__item">
+                <input type="checkbox">
+                <div>teste de item 1</div>
+                <input type="button" value="X">
+            </label> */
+const criarItem = () => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox">
+        <div>teste de item 1</div>
+        <input type="button" value="X">
+    `
+    document.getElementById('todoList').appendChild(item);
+}
